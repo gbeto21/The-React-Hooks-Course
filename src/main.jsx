c7dae6fde7a96bcb5ee6c9fe3d21b8aef18ff53d
Alberto <albertoglezb@hotmail.com>
2023-11-04 14:47:43 -0600

    UseEffect example with clean up.

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import HookUseState from './hooks/UseState.jsx';
+import HooksUseEffect from './hooks/UseEffect.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HookUseState />
+    <HooksUseEffect/>
   </React.StrictMode>
 );
