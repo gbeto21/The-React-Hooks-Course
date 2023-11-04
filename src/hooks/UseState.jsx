e0514b34a0d8923fcbd2c730e896d755ae1e0bd2
Alberto <albertoglezb@hotmail.com>
2023-11-04 14:01:30 -0600

    Example of the function to set a state.

--- src/hooks/UseState.jsx
+++ src/hooks/UseState.jsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useState } from 'react';
+import './Hook.css';
 
 const HookUseState = () => {
-    return (
-        <></>
-    )
+  const [count, setCount] = useState(0);
+  let localVariable = 0;
 
-}
+  const handCount = () => {
+    console.log('before: ' + localVariable);
+    setCount((prevState) => prevState + 1);
+    setCount((prevState) => prevState + 1);
+    setCount((prevState) => prevState + 1);
+    localVariable = count;
+    console.log('after: ' + localVariable + '\n\n');
+  };
 
-export default HookUseState
+  return (
+    <>
+      <h1>Hooks: useState</h1>
+      <div className="card">
+        <button className="text" onClick={handCount}>
+          Count is {count}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default HookUseState;
